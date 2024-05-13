6f88f3fc91ecba30aea39ae4e50fde6ae403cfc4
Nicolas Perez <nicoperez01@gmail.com>
2024-05-12 19:29:37 -0500

    Basis for OS

---
+++ app/components/OS-background.js
@@ -0,0 +1,11 @@
+import classes from "./OS-background.module.css";
+
+function OsBackground(props) {
+    return (
+        <div className={classes.background}>
+
+        </div>
+    );
+}
+
+export default OsBackground;
---
+++ app/components/OS-taskbar.js
@@ -0,0 +1,11 @@
+import classes from "./OS-taskbar.module.css";
+
+function OsTaskbar(props) {
+    return (
+        <div className={classes.taskbar}>
+
+        </div>
+    );
+}
+
+export default OsTaskbar;
